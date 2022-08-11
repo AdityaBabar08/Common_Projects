e3a62a89202a55594831b1d9ed9a7e593083b287
AdityaBabar08 <92012910+AdityaBabar08@users.noreply.github.com>
2022-08-11 12:10:33 +0530

    Update Show.java

--- Show.java
+++ Show.java
@@ -1,12 +1,12 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Arrays;
 import java.util.List;
 
 public class Show extends Records {
 
-    //List<String> headersList = Arrays.asList("NAME", "GENDER", "MARRIED", "AGE", "SALARY($)");
+    List<String> headersList = Arrays.asList("ID", "NAME", "MOBILE", "LOCATION", "COURSE");
+    List<List<String>> rowList;
     String currentLine;
     String value[];
 
@@ -29,8 +29,20 @@ public class Show extends Records {
 
             while ((currentLine = br.readLine())!= null){
             setValue(currentLine.split(","));
+             rowList = Arrays.asList(Arrays.asList(getValue()));
             }
-            System.out.println(value[0] + value[1] + value[2] + value[3] + value[4]);
+            //System.out.println(value[0] + value[1] + value[2] + value[3] + value[4]);
+            wagu.Board board = new wagu.Board(75);
+            wagu.Table table = new wagu.Table(board, 75, headersList, rowList);
+            List<Integer> colWidthsList = Arrays.asList(14, 14, 13, 14, 14);
+            table.setColWidthsList(colWidthsList);
+            wagu.Block tableBlock = table.tableToBlocks();
+            board.setInitialBlock(tableBlock);
+            board.build();
+            String preview1 = board.getPreview();
+            System.out.println("TABLE EXAMPLE 1");
+            System.out.println(preview1);
+
 
         }
         catch (Exception e){
