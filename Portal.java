d279cc6992969a9ab48192e0c570bd86853671f6
AdityaBabar08 <92012910+AdityaBabar08@users.noreply.github.com>
2022-08-11 19:52:04 +0530

    Update Portal.java

--- Portal.java
+++ Portal.java
@@ -1,15 +1,16 @@
 public class Portal extends Records {
+    Show s = new Show();
+    OperationsOnRecords o = new OperationsOnRecords();
 
     void menu() {
         System.out.println("1 : REGISTER NEW STUDENT \n2 : DELETE RECORD \n3 : SHOW RECORD \n4 : SEARCH \n5 : LOGOUT");
         int op = sc.nextInt();
         switch (op) {
-            case 1 -> Register();
-            case 2 -> //delete();
-            case 3 -> //show();
-            case 4 -> //search()
-            case 5 -> System.out.println("LOGGED OUT");
-            System.exit(0);
+            case 1 : Register();break;
+            case 2 : o.Delete();break;
+            case 3 : s.showRecord();break;
+            case 4 : o.search();break;
+            case 5 : System.out.println("LOGGED OUT");System.exit(0);
 
 
         }
